07284b8a458c86667c8bfb483c69af432313a3ae
Michal3333 <michalrajczak33@gmail.com>
2021-05-05 21:05:37 +0200

    update notifications

--- API/notifications.ts
+++ API/notifications.ts
@@ -14,21 +14,21 @@ export async function createContributorInvitation(notification: NotificationOut,
          .withConverter(notificationOutConverter)
          .add(notification)
    } else {
-      console.log('notification error')
+      throw new Error("No user with such a email");
    }
    return true;
 }
 
 export async function keepGettingNotifications(userId: string, addNotification : (notifications: NotificationIn) => void) {
    const db = firebase.firestore();
-   const unsubsrcibe =  db.collection(FB_COLLECTIONS.USERS)
+   const unsubsrcibe = db.collection(FB_COLLECTIONS.USERS)
       .doc(userId)
       .collection(FB_COLLECTIONS.IN_NOTIFICATIONS)
       .withConverter(notificationInConverter)
       .onSnapshot(snapShots => {
-         snapShots.forEach(x => {
-            if(x.exists){
-               const data = x.data();
+         snapShots.docChanges().forEach(x => {
+            if (x.type === "added") {
+               const data = x.doc.data()
                addNotification(data)
             }
          })
