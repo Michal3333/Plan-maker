ac28763564c3f6a559023604bccddd430fc358e6
Michal3333 <michalrajczak33@gmail.com>
2021-05-25 12:38:30 +0200

    update project box

--- components/MyProjects/ProjectBox.tsx
+++ components/MyProjects/ProjectBox.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native'
+import Card from '../UI/Card';
+import ThemedLabel from '../UI/ThemedLabel';
 
 type Props = {
    id: string,
    name: string,
-   openDetails: () => void
+   openDetails: () => void,
+   darkMode: boolean
 }
 
 const ProjectBox = (props : Props) => {
    return (
-      <View style={styles.box}>
-         <Text>{props.id}</Text>
-         <Text>{props.name}</Text>
+      <Card darkMode={props.darkMode}>
+         <View style={styles.projectName} >
+            <ThemedLabel  darkMode={props.darkMode}>{props.name}</ThemedLabel>
+         </View>
+         
          <View>
             <Button title="Details" onPress={props.openDetails}/>
          </View>
-      </View>
+      </Card>
+
    )
 }
 
 const styles = StyleSheet.create({
-   box: {
-      marginTop: 20,
-      width: '100%',
-      alignItems: 'center'
+   projectName: {
+      width: '100%'
    }
 })
 
